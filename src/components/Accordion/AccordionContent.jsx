ae9652429c8e42696dab99fee5a635544e49af63
ShunTr <repaco9@gmail.com>
2024-10-20 11:50:14 +0200

    Adding components for reusability

---
+++ src/components/Accordion/AccordionContent.jsx
@@ -0,0 +1,8 @@
+import { useAccordionContext } from './Accordion.jsx'
+
+export default function AccordionContent({ id, className, children }) {
+    const { openItemId } = useAccordionContext()
+    const isOpen = openItemId === id
+
+    return <div className={isOpen ? `${className ?? ''} open` : `${className ?? ''} close`}>{children}</div>
+}
